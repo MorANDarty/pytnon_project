0c10eb3eaa612303bcb7a4b2fca86bbe996daa24
ilmirka <shgbievi@gmail.com>
2020-06-02 19:32:11 +0300

    path handling

--- LinksShortenerApp/views.py
+++ LinksShortenerApp/views.py
@@ -7,3 +7,10 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello World!")
+
+
+def check_and_redirect(request, url_hash):
+    return HttpResponse('U are try to go /'+str(url_hash))
+
+
+
--- untitled1/urls.py
+++ untitled1/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import request
+from django.urls import path, re_path
 
 from LinksShortenerApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('urls/<str:url_hash>/', views.check_and_redirect)
 ]
